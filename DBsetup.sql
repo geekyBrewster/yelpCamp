809543e9370f5daca26fbf0861d00f56bced12c3
Amanda Kirchner <geeky.brewster@gmail.com>
2017-11-01 12:01:41 -0500

    Added route for showing specific campsite details. Added description to the DB and new campground form.

--- DBsetup.sql
+++ DBsetup.sql
@@ -4,19 +4,25 @@ CREATE TABLE "campsites" (
   "id" serial primary key,
   "name" varchar(120) not null,
   "image" varchar(200) not null,
+  "description" varchar(350) null
 )
 
-INSERT INTO "campsites" ("name", "image")
-VALUES ('Salmon Creek', 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg');
+INSERT INTO "campsites" ("name", "image", "description")
+VALUES ('Salmon Creek', 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg',
+'Features two separate creeks which are good for fishing');
 
-INSERT INTO "campsites" ("name", "image")
-VALUES ('Redwood Falls', 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg');
+INSERT INTO "campsites" ("name", "image", "description")
+VALUES ('Redwood Falls', 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg',
+'Primative campsites available. Other campsites feature modern amenities');
 
-INSERT INTO "campsites" ("name", "image")
-VALUES ('Chase River Woods', 'https://farm5.staticflickr.com/4027/4368764673_c8345bd602.jpg');
+INSERT INTO "campsites" ("name", "image", "description")
+VALUES ('Chase River Woods', 'https://farm5.staticflickr.com/4027/4368764673_c8345bd602.jpg',
+'Modern amenities. Sites available for pullthrough trailers.');
 
-INSERT INTO "campsites" ("name", "image")
-VALUES ('Rice Lake', 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg');
+INSERT INTO "campsites" ("name", "image", "description")
+VALUES ('Rice Lake', 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg',
+'Primative campsites only. Water pumps near entrance.');
 
-INSERT INTO "campsites" ("name", "image")
-VALUES ('Granite Hills', 'https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg');
+INSERT INTO "campsites" ("name", "image", "description")
+VALUES ('Granite Hills', 'https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg',
+'Modern amenities. Wood available for purchase at ranger station.');
--- server/server.js
+++ server/server.js
@@ -19,15 +19,15 @@ app.get('/campgrounds', function(req, res){
       res.sendStatus(500);
     } else {
       // MAKE DB QUERY
-      db.query('SELECT name, image FROM campsites;',
+      db.query('SELECT * FROM campsites;',
       function(errMakingQuery, result){
         done();
         if(errMakingQuery){
           console.log('There was an error making INSERT query: ', errMakingQuery);
           res.sendStatus(500);
         } else {
-          //console.log('Retrieved client data from DB: ', result);
-          res.render("campgrounds", {campgrounds: result.rows}); //second variable is data object passed to client side
+          console.log('Retrieved campground data from DB: ', result.rows);
+          res.render("index", {campgrounds: result.rows}); //second variable is data object passed to client side
         }
       });
     } // end of else
@@ -39,6 +39,7 @@ app.post('/campgrounds', function(req, res){
   //get data from form
   var name = req.body.name;
   var image = req.body.image;
+  var desc = req.body.description;
   //add to campgrounds DB
   pool.connect(function(errConnectingToDatabase, db, done){
     if(errConnectingToDatabase) {
@@ -46,7 +47,7 @@ app.post('/campgrounds', function(req, res){
       res.sendStatus(500);
     } else {
       // MAKE DB QUERY
-      db.query("insert into campsites(name, image) values($1, $2);", [name, image],
+      db.query("insert into campsites(name, image, description) values($1, $2, $3);", [name, image, desc],
       function(errMakingQuery, result){
         done();
         if(errMakingQuery){
@@ -54,19 +55,44 @@ app.post('/campgrounds', function(req, res){
           res.sendStatus(500);
         } else {
           console.log('Campsite added');
-          //res.sendStatus(200);
           //redirect back to campgrounds page
           res.redirect('/campgrounds');
         }
       });
     } // end of else
   }); //end of pool.connect
-
-});
+}); //end of POST route
 
 app.get('/campgrounds/new', function(req, res){
   res.render('new.ejs');
 });
+
+app.get('/campgrounds/:id', function(req, res){
+  //Find campground with matching id
+  var campsiteID = req.params.id;
+  //find campsite in DB
+  pool.connect(function(errConnectingToDatabase, db, done){
+    if(errConnectingToDatabase) {
+      console.log('There was an error connecting to database: ', errConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      // MAKE DB QUERY
+      db.query("select * from campsites where id=$1;", [campsiteID],
+      function(errMakingQuery, result){
+        done();
+        if(errMakingQuery){
+          console.log('There was an error making INSERT query: ', errMakingQuery);
+          res.sendStatus(500);
+        } else {
+          console.log('Campsite added');
+          //Render template to display that campgrond's info
+          console.log('Retrieved campsite data from DB: ', result.rows);
+          res.render("show", {campsite: result.rows});
+        }
+      });
+    } // end of else
+  }); //end of pool.connect
+}); //end of GET single campsite
 
 app.get('/', function(req, res) {
   console.log('request for landing page');
